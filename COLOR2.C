2d51b4907a2ca7ae6f35d07537cfd8109d13b1cd
elight <minghir@gmail.com>
2022-08-05 12:56:24 +0300

    Add files via upload

--- COLOR2.C
+++ COLOR2.C
@@ -15,22 +15,37 @@
 
 #define MAX_ZOOM 5
 
-int zoom = 30;
+int zoom = 15;
 float a_ratio = 1.2;
 unsigned long int tstart,tend, fps_counter = 0, fps_count = 0;
 int delay_msec = 200;
 int grid = 1;
 
+int colors[6];
+int table[100];
+int current_color;
+int old_color;
 
 int pan_vert = 5;
 int pan_horiz = 60;
 
 void initialize(void)
 {
+	
     initialize_graphics(true);
     ctrlbrk(ctrlbrk_handler);
     hook_keyb_int();
     init_chars_15();
+	
+	colors[0] = 42;
+	colors[1] = 35;
+	colors[2] = 52;
+	colors[3] = 49;
+	colors[4] = 27;
+	colors[5] = 44;
+	
+	
+
 }
 
 
@@ -58,43 +73,126 @@ void clear_screen(){
 
 
 void draw(){
-        int i,x,y;
+        int i,x,y,c;
         char buf[5];
         clear_screen();
         
+		rect_fill(0,zoom,(zoom)*a_ratio,zoom*2,current_color);
+		rect_fill(0,zoom*2,(zoom)*a_ratio,zoom*3,old_color);
+		
 		x=pan_horiz;
 		y=pan_vert;
 
-        for( i= 1; i<=25; i++){
-                rect_fill(x*a_ratio,y,(x+zoom)*a_ratio,y+zoom,100+i);
-                if(i%5 == 0){
+        for( i= 1; i<=100; i++){
+				rect_fill(x*a_ratio,y,(x+zoom)*a_ratio,y+zoom,table[i-1]);
+                if(i%10 == 0){
                         y += zoom;
                         x = pan_horiz;
                 }else{
                         x += zoom;
                 }
         }
+		
+		x=pan_horiz+30;
+		y = 170;
+		for( i = 0; i<6;i++){
+			rect_fill(x*a_ratio,y,(x+zoom)*a_ratio,y+zoom,colors[i]);
+			x +=zoom;
+		}
+		
+		
 }
 
+void init_table(){
+	int i;
+	for (i = 0; i<100;i++){
+		table[i] = colors[random(6)];
+	}
+	current_color = table[0];
+	old_color = current_color;
+}
 
+bool check_solution( ){
+	int i = 0;
+	int color = table[0];
+	
+	for (i = 1 ; i<100; i++)
+		if(table[i] != color)
+			return false;
+	
+	return true;
+}
+
+void calculate_table(){
+	int i;
+	table[0] = current_color;
+	for(i = 1;i<100;i++){
+		if(table[i] == current_color) continue;
+		if(table[i] == old_color) table[i] = current_color;
+	}
+}
 
 int main(){
-
         initialize();
 		randomize(); 
-
+		init_table();
                 while(1){
                     if(get_key_status(SCAN_Q) || get_key_status(SCAN_ESC)){
                         break;
 					}
+					if(get_key_status(SCAN_R)){
+                        init_table();
+						draw();
+						delay(200);
+					}
+					
+					if(get_key_status(SCAN_1)){
+						old_color=current_color;
+						current_color = colors[0];
+                        calculate_table();
+						delay(200);
+					}
+					
+					if(get_key_status(SCAN_2)){
+						old_color=current_color;
+						current_color = colors[1];
+                        calculate_table();
+						delay(200);
+					}
+					
+					if(get_key_status(SCAN_3)){
+						old_color=current_color;
+						current_color = colors[2];
+                        calculate_table();
+						delay(200);
+					}
+					
+					if(get_key_status(SCAN_4)){
+						old_color=current_color;
+						current_color = colors[3];
+                        calculate_table();
+						delay(200);
+					}
+					
+					if(get_key_status(SCAN_5)){
+						old_color=current_color;
+						current_color = colors[4];
+                        calculate_table();
+						delay(200);
+					}
+					
+					if(get_key_status(SCAN_6)){
+						old_color=current_color;
+						current_color = colors[5];
+                        calculate_table();
+						delay(200);
+					}
+					
 
                         draw();
                         show_buffer();
                 }
-
-
                 unhook_keyb_int();
-
                 delay(100);
                 exit_graphic();
                 exit(0);
