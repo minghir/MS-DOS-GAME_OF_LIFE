4ae7fdf5e16c36c8149e2babe53dbb03c6a8eec2
elight <minghir@gmail.com>
2022-08-18 16:18:45 +0300

    Add files via upload

--- colorl.c
+++ colorl.c
@@ -9,13 +9,14 @@
 #include "LVGA\\lvga.h"
 #include "LVGA\\keyb.h"
 
-
 #define LEFT 0
 #define RIGHT 1
 #define UP 2
 #define DOWN 3
 
-#define MWHITE  30
+#define MWHITE  29
+
+bool hard_mode = true;
 
 int zoom = 30;
 float a_ratio = 1.2;
@@ -26,10 +27,11 @@ int table[100];
 int pan_vert = 13;
 int pan_horiz = 60;
 
-int clicks = 0;
+int score = 0;
 
 int line_found = -1;
 int col_found = -1;
+bool five_found = false;
 
 int selector = 24;
 int selected_cell = -1;
@@ -50,15 +52,9 @@ void initialize(void){
 
 void print_score(){
 	char fps_c[10];
-	sprintf(fps_c,"%u",clicks );
+	sprintf(fps_c,"%u",score );
 	put_str_15(160,170,fps_c,MWHITE,240);	
 }
-
-void clear_screen(){
-        //rect_fill(0,0, 319, 199, BLACK);
-		rect_fill(0,0, 319, 199, 17);
-}
-
 
 
 void draw_selection(){
@@ -73,7 +69,7 @@ void draw_selection(){
 
 void draw(){
         int i,x,y;
-        clear_screen();
+        rect_fill(0,0, 319, 199, 20); // clear screen
         
 		x=pan_horiz;
 		y=pan_vert;
@@ -107,51 +103,25 @@ void init_table(){
 	table[24] = 0;
 /*
 	table[0] = colors[1];
-	table[1] = colors[1];
-	table[2] = colors[1];
-	table[3] = colors[1];
-*/	
+	table[5] = colors[1];
+	table[10] = colors[1];
+	table[15] = colors[1];
+	table[20] = colors[1];
+*/
 	line_found = -1;
 	col_found = -1;
 
 }
 
-int check_table_lines(){
-	int i;
-	for(i = 0; i <= 20 ; i += 5){
-		if( (table[i] == table[i+1]) && (table[i] == table[i+2]) && (table[i] == table[i+3] )){
-			return i;
-		}
-		if( (table[i+1] == table[i+2]) && (table[i+1] == table[i+3]) && (table[i+1] == table[i+4] )){
-			return i+1;
-		}
-	}
-	
-	return -1;
-}
-
-int check_table_cols(){
-	int i;
-	for(i = 0; i <= 4 ; i++){
-		if( (table[i] == table[i+5]) && (table[i] == table[i+10]) && (table[i] == table[i+15] )){
-			return i;
-		}
-		if( (table[i+5] == table[i+10]) && (table[i+5] == table[i+15]) && (table[i+5] == table[i+20] )){
-			return i+5;
-		}
-	}
-	
-	return -1;
-}
-
 void flick_square(int x1, int y1, int x2, int y2,int cl){
 		int i=0;
 		int color;
-		while( i!=5){
-			color = color == cl ? MWHITE : cl;
+		int flick_color = 44;
+		while( i!=6){
+			color = color == cl ? flick_color : cl;
 			
 			rect_fill(x1*a_ratio,y1,x2*a_ratio,y2,color);	
-			rect_fast(x1*a_ratio,y1,x2*a_ratio,y2,(color == cl?MWHITE:cl));	
+			rect_fast(x1*a_ratio,y1,x2*a_ratio,y2,(color == cl?flick_color:cl));	
 			show_buffer();
 			
 			if(color == cl)
@@ -164,41 +134,118 @@ void flick_square(int x1, int y1, int x2, int y2,int cl){
 }
 
 void replace_line(){
-	int i,rnd;
-	flick_square((pan_horiz+line_found%5*zoom),(pan_vert+(line_found/5)*zoom), (pan_horiz+line_found%5*zoom + 4*zoom),(pan_vert+(line_found/5)*zoom)+zoom,table[line_found]);
-
-	for(i = 0; i<4;i++){
+	int i,no;
+	no = five_found ? 5 : 4;
+	flick_square((pan_horiz+line_found%5*zoom),(pan_vert+(line_found/5)*zoom), (pan_horiz+line_found%5*zoom + no*zoom),(pan_vert+(line_found/5)*zoom)+zoom,table[line_found]);
+	
+	for(i = 0; i<no;i++){
+		table[line_found+i] = 0;
+	}
+	draw();
+	show_buffer();
+	delay(120);
+	
+	for(i = 0; i<no;i++){
 		table[line_found+i] = colors[random(5)];
 		draw();
 		show_buffer();
-		//delay(120);
+		delay(120);
 	}
 	
 	line_found = -1;
+	five_found = false;
 }
 
 void replace_col(){
-	int i;
-	flick_square((pan_horiz+col_found%5*zoom),pan_vert+(col_found>4?zoom:0), (pan_horiz+col_found%5*zoom+zoom),pan_vert+4*zoom+(col_found>4?zoom:0),table[col_found]);		
-	for(i=0;i<4;i++){
+	int i,no;
+	no = five_found ? 5 : 4;
+	flick_square((pan_horiz+col_found%5*zoom),pan_vert+(col_found>4?zoom:0), (pan_horiz+col_found%5*zoom+zoom),pan_vert+no*zoom+(col_found>4?zoom:0),table[col_found]);		
+	
+	for(i=0;i<no;i++){
+		table[col_found+(i*5)] = 0;
+	}
+	
+	draw();
+	show_buffer();
+	delay(120);
+	
+	for(i=0;i<no;i++){
 		table[col_found+(i*5)] = colors[random(5)];
 		draw();
 		show_buffer();
-		//delay(120);
+		delay(120);
 	}
 	col_found = -1;
+	five_found = false;
+}
+
+
+int check_table_lines(){
+	int i;
+	for(i = 0; i <= 20 ; i += 5){
+		if(hard_mode) {
+			if( (table[i] == table[i+1]) && (table[i] == table[i+2]) && (table[i] == table[i+3] && (table[i] == table[i+4] ))){
+					five_found = true;
+				return i;
+			}
+		}else{
+			if( (table[i] == table[i+1]) && (table[i] == table[i+2]) && (table[i] == table[i+3] )){
+				if(table[i] == table[i+4] )
+					five_found = true;
+				return i;
+			}
+			if( (table[i+1] == table[i+2]) && (table[i+1] == table[i+3]) && (table[i+1] == table[i+4] )){
+				return i+1;
+			}
+		}
+	}
+	
+	return -1;
+}
+
+int check_table_cols(){
+	int i;
+	for(i = 0; i <= 4 ; i++){
+		if(hard_mode) {
+			if( (table[i] == table[i+5]) && (table[i] == table[i+10]) && (table[i] == table[i+15] ) && (table[i] == table[i+20])){
+				five_found = true;
+				return i;
+			}
+		}else{
+			if( (table[i] == table[i+5]) && (table[i] == table[i+10]) && (table[i] == table[i+15] )){
+				if(table[i] == table[i+20])
+					five_found = true;
+				return i;
+			}
+			if( (table[i+5] == table[i+10]) && (table[i+5] == table[i+15]) && (table[i+5] == table[i+20] )){
+				return i+5;
+			}
+		}
+	}
+	
+	return -1;
 }
 
 void check(){
 	line_found = check_table_lines();
 	if(line_found > -1){
 		replace_line();
-		clicks++;
+		if(!hard_mode && five_found)
+			score +=2;
+		else
+			score++;
+			
+		five_found = false;
 	}
 	col_found = check_table_cols();
 	if(col_found > -1){
 		replace_col();
-		clicks++;
+		if(!hard_mode && five_found)
+			score +=2;
+		else
+			score++;
+		
+		five_found = false;
 	}
 }
 
@@ -242,6 +289,8 @@ void move_cell(int pos){
 bool usr_input(){
 
 					if(get_key_status(SCAN_Q) || get_key_status(SCAN_ESC)){
+						while(get_key_status(SCAN_ESC));
+						while(get_key_status(SCAN_Q));
                         return false;
 					}
 					
@@ -252,7 +301,7 @@ bool usr_input(){
 								selected_cell = -1;
 							else
 								selected_cell = selector;
-							check();
+						
 						return true;
 					}
 					
@@ -265,7 +314,7 @@ bool usr_input(){
 						draw();	
 						show_buffer();
 						while(get_key_status(SCAN_LEFT_ARROW)){}
-						check();
+						
 						return true;
 					}
 					
@@ -279,7 +328,7 @@ bool usr_input(){
 						draw();	
 						show_buffer();
 						while(get_key_status(SCAN_RIGHT_ARROW)){}
-						check();
+						
 						return true;
 					}
 					
@@ -293,7 +342,7 @@ bool usr_input(){
 						draw();	
 						show_buffer();
 						while(get_key_status(SCAN_UP_ARROW)){}
-						check();
+						
 						return true;
 					}
 					
@@ -307,23 +356,34 @@ bool usr_input(){
 						draw();	
 						show_buffer();
 						while(get_key_status(SCAN_DOWN_ARROW)){}
-						check();
+						
 						return true;
 					}
 					
 	return true;
 }
 
-int main(){
-        initialize();
+int main(int argc, char *argv[]){
+
+	
+		if(argc > 1)
+			if(atoi(argv[1])==1)
+				hard_mode = false;
+			else if(argv[1][0]=='h' || argv[1][0]=='?'){
+					printf("use:\n%s 1 for easy mode\n",argv[0]);
+					printf("%s ? for this message\n",argv[0]);
+					return 1;
+				}
+
+		initialize();
 		randomize(); 
 		init_table();
 		check();
-                while(1){
-                 					
+		
+			    while(1){
 					if(!usr_input())
 						break;
-
+					check();
 					draw();
                     show_buffer();
                 }
